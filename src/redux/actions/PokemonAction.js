00e5af00a61990bd8750b257cd39e30088aefe84
Mert Yıdırır <mertyildirir14@gmail.com>
2021-08-01 02:42:36 +0200

    action and reducer for listing pokemons added

---
+++ src/redux/actions/PokemonAction.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+
+export const GetPokemonList = (page) => async dispatch => {
+    try {
+        dispatch({
+            type: "POKEMON_LIST_LOADING"
+        });
+
+        const perPage = 15;
+        const offset = (page * perPage) - perPage; 
+
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${perpage}&offset=${offset}`);
+
+        dispatch({
+            type: "POKEMON_LIST_SUCCESS",
+            payload: res.data
+        });
+    } catch (e) {
+        dispatch({
+            type: "POKEMON_LIST_FAIL"
+        });
+    }
+}
---
+++ src/redux/reducers/PokemonListReducer.js
@@ -0,0 +1,32 @@
+const initialState = {
+    data: [],
+    loading: false,
+    errorMessage: ""
+};
+
+const PokemonListReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "POKEMON_LIST_LOADING":
+            return {
+                ...state,
+                loading: true
+            };
+        case "POKEMON_LIST_FAIL":
+            return {
+                ...state,
+                loading: false,
+                errorMessage: "unable to get pokemon"
+            };
+    
+        case "POKEMON_LIST_SUCCESS":
+            return {
+                ...state,
+                loading: false,
+                data: action.payload
+            };
+        default:
+            return state
+    }
+}
+
+export default PokemonListReducer;

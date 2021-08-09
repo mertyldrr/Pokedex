be69d777b51b43a442d0339c3adb9d9a68727d6e
Mert Yıdırır <mertyildirir14@gmail.com>
2021-08-09 23:48:08 +0200

    search bar centered, react-icons installed

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
-import { Navbar, Nav, Form, Button, FormControl, NavbarBrand } from 'react-bootstrap';
+import { Navbar, Form, Button, FormControl, NavbarBrand } from 'react-bootstrap';
 import logo from '../resources/pokedex-logo.png';
 
 
@@ -10,28 +10,16 @@ const NavigationBar = () => {
   const history = useHistory();
 
   return (
-    <Navbar style={{ backgroundColor: "#D3D3D3" }}>
-      <Nav className="d-flex justify-content-start">
-        <NavbarBrand href="/">
-          <img src={logo} alt="pokedex-logo" style={{ width: 250, marginLeft: 20 }} />
-        </NavbarBrand>
-      </Nav>
-      <Nav className="d-flex flex-grow-1 justify-content-center">
-        <Form className="d-inline-flex">
-          <FormControl
-            type="search"
-            placeholder="Type pokemon name"
-            value={search}
-            onChange={e => setSearch(e.target.value)}
-          />
-          <Button
-            style={{ backgroundColor: "#ffe031", color: "#396bba" }}
-            onClick={() => history.push(`/pokemon/${search}`)}
-          >
-            <strong>Search</strong>
-          </Button>
-        </Form>
-      </Nav>
+    <Navbar className="d-flex position-relative" style={{ backgroundColor: "#D3D3D3" }}>
+      <NavbarBrand href="/" style={{ float: 'left' }}>
+        <img src={logo} alt="pokedex-logo" style={{ width: 250, marginLeft: 20 }} />
+      </NavbarBrand>
+      <Form className="d-flex position-absolute" style={{ top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+        <FormControl type="search" placeholder="Type pokemon name" value={search} onChange={e => setSearch(e.target.value)} />
+        <Button style={{ backgroundColor: "#ffe031", color: "#396bba" }} onClick={() => history.push(`/pokemon/${search}`)}>
+          <strong>Search</strong>
+        </Button>
+      </Form>
     </Navbar>
   )
 }
